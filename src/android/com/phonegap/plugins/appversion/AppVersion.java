1ccda0a7613d84a690386ce9a9f8d4d7ae425fa6
MuaDDuB <muaddub@local.local>
2013-08-30 11:40:26 +0200

    Android commit
    Add the Android part of the plugin

---
+++ src/android/com/phonegap/plugins/appversion/AppVersion.java
@@ -0,0 +1,36 @@
+package org.apache.cordova.plugin.Version;
+
+import org.json.JSONArray;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.api.PluginResult.Status;
+
+public class AppVersion extends CordovaPlugin {
+
+    public final String ACTION_GET_VERSION_NUMBER = "getVersionNumber";
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        boolean result = false;
+        PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        if(action.equals(ACTION_GET_VERSION_NUMBER)) {
+            try {
+                PackageInfo packageInfo = packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0);
+                result = true;
+                callbackContext.success(packageInfo.versionCode);
+            }
+            catch (NameNotFoundException exception) {
+                result = false;
+                callbackContext.success(exception.getMessage());
+            }
+        }
+
+        return result;
+    }
+}
